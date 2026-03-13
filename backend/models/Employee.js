fb4d637c4fc8ffad0d0273a18e5ed9f65f0e517b
vidhi0212-cloud <ranavidhi1234567@gmail.com>
2026-03-13 22:09:10 +0530

    Update Employee.js

--- backend/models/Employee.js
+++ backend/models/Employee.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 
 const EmployeeSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  department: String,
-  token: String,
-  clicked: { type: Boolean, default: false }
+
+name: String,
+email: String,
+department: String,
+
+token: String,
+
+clicked: {
+type: Boolean,
+default: false
+}
+
 });
 
 module.exports = mongoose.model("Employee", EmployeeSchema);
