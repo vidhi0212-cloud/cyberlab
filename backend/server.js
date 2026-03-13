784cbbd6e5cccfdaf3eb27efd0ded1388e4d21b0
vidhi0212-cloud <ranavidhi1234567@gmail.com>
2026-03-13 21:42:16 +0530

    Set up Express server with MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const authRoutes = require("./routes/authRoutes");
+const groupRoutes = require("./routes/groupRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/cyberlab");
+
+app.use("/auth", authRoutes);
+app.use("/groups", groupRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
