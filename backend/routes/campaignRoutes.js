3fecd646b2d251b996cbba86130555bdba9507e5
vidhi0212-cloud <ranavidhi1234567@gmail.com>
2026-03-13 21:52:37 +0530

    Refactor campaign creation and delivery routes

--- backend/routes/campaignRoutes.js
+++ backend/routes/campaignRoutes.js
@@ -2,22 +2,60 @@ const router = require("express").Router();
 const Campaign = require("../models/Campaign");
 const Employee = require("../models/Employee");
 
-router.post("/create", async (req,res)=>{
+const generateToken = require("../controllers/tokenGenerator");
+const templates = require("../controllers/templates");
 
-const {campaignName,attacks,departments,template} = req.body;
+router.post("/create", async (req, res) => {
 
-const campaign = new Campaign({
+  const { campaignName, attacks, departments } = req.body;
 
-campaignName,
-attacks,
-departments,
-template
+  const campaign = new Campaign({
+    campaignName,
+    attacks,
+    departments
+  });
+
+  await campaign.save();
+
+  res.json("Campaign Created");
 
 });
 
-await campaign.save();
 
-res.json("Campaign Created");
+// Simulation delivery
+
+router.post("/deliver", async (req,res)=>{
+
+const {attack, departments} = req.body;
+
+const selectedTemplate = templates[attack];
+
+const employees = await Employee.find({
+department: { $in: departments }
+});
+
+let result = [];
+
+for(let emp of employees){
+
+const token = generateToken();
+
+emp.token = token;
+await emp.save();
+
+result.push({
+
+name: emp.name,
+email: emp.email,
+department: emp.department,
+token: token,
+template: selectedTemplate.subject
+
+});
+
+}
+
+res.json(result);
 
 });
 
